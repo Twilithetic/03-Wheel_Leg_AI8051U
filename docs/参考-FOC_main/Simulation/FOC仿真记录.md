65c1882b6c5345bf86a73d8332de2ff57a715732
twilithetic <2934481106@qq.com>
2026-05-23 11:59:21 +0800

    Remove docs/ from index (preparing for submodule)

--- docs/参考-FOC_main/Simulation/FOC仿真记录.md
+++
@@ -1,5 +0,0 @@
-# FOC仿真记录
-
-## 仿真软件使用
-
-> [Simulink快速入门](https://ww2.mathworks.cn/help/simulink/getting-started-with-simulink.html)
--- docs/参考-FOC_main/Software/FOC_Driver_backup/cmake-build-debug-mingw-stm32/CMakeFiles/FOC_Driver.elf.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for FOC_Driver.elf.
